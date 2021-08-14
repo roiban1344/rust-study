f1edb17b71e4b922543e9fe2667cfb7dd80e024e
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-15 01:21:06 +0900

    Add explicit type conversion

--- projects/sieve/src/main.rs
+++ projects/sieve/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let n = 100;
-    let mut is_prime = vec![true; n + 1];
+    let n: u32 = 100;
+    let mut is_prime = vec![true; (n + 1) as usize];
     let mut primes = vec![];
     for d in 2..=n {
-        if is_prime[d] {
+        if is_prime[d as usize] {
             primes.push(d);
-            for i in (2 * d..=n).step_by(d) {
-                is_prime[i] = false;
+            for i in (2 * d..=n).step_by(d as usize) {
+                is_prime[i as usize] = false;
             }
         }
     }

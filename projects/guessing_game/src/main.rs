ef60eb03a636011481582cbdf4bee8ba7a9f6e91
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-14 23:35:11 +0900

    Add error handling for invalid input

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,7 +9,7 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    loop{
+    loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -18,14 +18,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number){
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal =>{
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }

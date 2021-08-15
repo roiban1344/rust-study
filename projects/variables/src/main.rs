853d701fbe4f9897e3cdd7763dfdb4301eafcc0c
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-15 10:46:15 +0900

    The book chap.3: learn about shadowing

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
+    //mutability
+    //let x = 5;  //does not compile
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    //shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("The length of the spaces is: {}", spaces);
 }

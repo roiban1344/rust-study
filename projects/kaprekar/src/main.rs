7cef09f2cf658d6696d08f83d7308c3835c4a39b
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-16 07:37:55 +0900

    Rewrite Kaprekar transformation without string manipulation

--- projects/kaprekar/src/main.rs
+++ projects/kaprekar/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let mut x = 1729;
+    println!("{}", x);
     for _ in 0..10 {
         x = transform(x);
         println!("{}", x);
@@ -7,18 +8,21 @@ fn main() {
 }
 
 fn transform(x: i32) -> i32 {
-    let mut chars: Vec<char> = x.to_string().chars().collect();
-    chars.sort();
-    let max = chars
-        .iter()
-        .rev()
-        .collect::<String>()
-        .parse::<i32>()
-        .expect("error");
-    let min = chars
-        .iter()
-        .collect::<String>()
-        .parse::<i32>()
-        .expect("error");
-    max - min
+    let mut a = Vec::<i32>::new();
+    let mut x = x;
+    while x > 0 {
+        let q = x / 10;
+        a.push(x - q * 10);
+        x = q;
+    }
+    a.sort();
+    let mut y = 0;
+    for e in a.iter().rev() {
+        y = y * 10 + e;
+    }
+    let mut z = 0;
+    for e in a.iter() {
+        z = z * 10 + e;
+    }
+    y - z
 }

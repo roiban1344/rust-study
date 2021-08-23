1154e21cf633454204cc2ee4c98a2e2be52f17d3
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-24 03:05:46 +0900

    Simplify the error handling with `?` operator

--- projects/read_file/read_file/src/main.rs
+++ projects/read_file/read_file/src/main.rs
@@ -21,15 +21,8 @@ fn main() {
 }
 
 fn read_user_name_from_file(file_name: &str) -> Result<String, io::Error> {
-    let mut f = match File::open(file_name) {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
+    let mut f = File::open(file_name)?;
     let mut s = String::new();
-
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }

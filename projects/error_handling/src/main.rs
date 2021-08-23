0c563ce2dea26ae247dd60784098b100e843478a
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-24 01:10:03 +0900

    Try invoking out-of-bound error of a vector

---
+++ projects/error_handling/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    //A053016 - OEIS https://oeis.org/A053016
+    let platonic_solid_vertices = vec![4, 6, 8, 12, 20];
+
+    //panics in the line below
+    println!("{}", platonic_solid_vertices[5]);
+
+
+}

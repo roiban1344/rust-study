7d05209f617be4dddf8532a7e7297ce777720140
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-24 01:19:07 +0900

    Take not-found errors into consideration with match expression

--- projects/read_file/read_file/src/main.rs
+++ projects/read_file/read_file/src/main.rs
@@ -1,10 +1,20 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
+    let file_name = "hello.txt";
+    let f = File::open(file_name);
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(file_name) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 }

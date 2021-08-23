ee934f69dc2399046721f6b9c9f6ad7df82d7750
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-24 01:33:25 +0900

    Rewrite error handling with closures

--- projects/read_file/read_file/src/main.rs
+++ projects/read_file/read_file/src/main.rs
@@ -3,18 +3,13 @@ use std::io::ErrorKind;
 
 fn main() {
     let file_name = "hello.txt";
-    let f = File::open(file_name);
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(file_name) {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        },
-    };
+    let f = File::open(file_name).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create(file_name).unwrap_or_else(|error| {
+                panic!("Problem crating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error)
+        }
+    });
 }

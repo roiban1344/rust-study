62347b4c821d425311851ea6acdc20a502c29a73
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-25 01:37:34 +0900

    Demand a trait implementation of `PartialOrd`

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,9 +1,11 @@
+use std::cmp::PartialOrd;
+
 fn main() {
     let list = [3, 2, 4, 5, 1];
     println!("The largest numer is {}", largest(&list).unwrap());
 }
 
-fn largest(list: &[i32]) -> Option<i32> {
+fn largest<T: PartialOrd>(list: &[T]) -> Option<T> {
     if list.len() == 0 {
         return None;
     }

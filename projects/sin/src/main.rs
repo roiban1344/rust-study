f7348f317ccf6ccad38c92ac7fd3af4edfbffd1a
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-28 04:44:43 +0900

    Implement addition, subtraction, multiplication with std::ops traits

--- projects/sin/src/main.rs
+++ projects/sin/src/main.rs
@@ -1,45 +1,28 @@
-use std::cmp::PartialEq;
-use std::cmp::Ordering;
-use num::integer;//::{div_ceil, div_floor};
+use num::integer;
+use std::ops::{Add,Sub,Mul};
 
 fn main() {
     let sin1 = Frac::new(Interval::new(7761199951101802512, 7761199951101802513));
     let cos1 = Frac::new(Interval::new(4983409179392355912, 4983409179392355913));
-    let sin2 = {
-        let tmp = sin1.prod(&cos1);
-        tmp.sum(&tmp)
-    };
-    let cos2 = cos1.prod(&cos1).sub(&sin1.prod(&sin1));
+    let sin2 = sin1 * cos1 + cos1 * sin1;
+    let cos2 = cos1 * cos1 - sin1 * sin1;
     println!("{:?}", sin2);
     println!("{:?}", cos2);
     println!("{:x}", integer::div_floor(sin1.num.min, 1<<31));
     println!("{:x}", integer::div_floor(sin2.num.min, 1<<31));
-    let sin3 = sin1.prod(&cos2).sum(&cos1.prod(&sin2));
-    let cos3 = cos1.prod(&cos2).sub(&sin1.prod(&sin2));
+    let sin3 = sin1*cos2 + cos1*sin2;
+    let cos3 = cos1*cos2-sin1*sin2;
     println!("{:x}", integer::div_floor(sin3.num.min, 1<<31));
-    let sin4 = sin2.prod(&cos2).sum(&cos2.prod(&sin2));
-    let cos4 = cos2.prod(&cos2).sub(&sin2.prod(&sin2));
+    let sin4 = sin2*cos2+cos2*sin2;
+    let cos4 = cos2*cos2-sin2*sin2;
     println!("{:x}", integer::div_floor(-sin4.num.min, 1<<31));
 }
 
-// #[derive(PartialEq)]
 #[derive(Copy, Clone, Debug)]
 struct Interval{
     min: i128,
     max: i128,
 }
-
-// impl PartialOrd for Interval{
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         if  self.max < other.min {
-//             Some(Ordering::Less)
-//         }else if self.min > other.max {
-//             Some(Ordering::Greater)
-//         } else {
-//             Some(Ordering::Equal)
-//         }
-//     }
-// }
 
 impl Interval{
     fn new(min:i128, max:i128) -> Interval{
@@ -57,9 +40,6 @@ impl Interval{
     fn is_positive(&self) -> bool{
         self.min > 0
     }
-    // fn is_negative(&self) -> bool{
-    //     self.max < 0
-    // }
 
     fn sum(&self, other: &Interval) -> Interval{
         Interval{
@@ -95,7 +75,7 @@ impl Interval{
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Frac{
     num: Interval
     //denom: 2^63
@@ -109,14 +89,25 @@ impl Frac{
             num
         }
     }
+}
 
-    fn sum(&self, other:&Self) -> Frac{
+impl Add for Frac{
+    type Output = Self;
+    fn add(self, other: Self) -> Frac{
         Frac::new(self.num.sum(&other.num))
     }
-    fn sub(&self, other:&Self)->Frac{
+}
+
+impl Sub for Frac{
+    type Output = Self;
+    fn sub(self, other:Self) -> Frac{
         Frac::new(self.num.sub(&other.num))
     }
-    fn prod(&self, other:&Self)->Frac{
+}
+
+impl Mul for Frac{
+    type Output = Self;
+    fn mul(self, other:Self) -> Frac{
         let num_prod = self.num.prod(&other.num);
         let floor = integer::div_floor(num_prod.min, DENOM);
         let ceil = integer::div_ceil(num_prod.max, DENOM);
@@ -124,4 +115,3 @@ impl Frac{
         Frac::new(interval)
     }
 }
-

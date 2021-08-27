5a6961ac0de4baf506a9822d246ca42664f5cf1e
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-08-28 05:01:31 +0900

    Define a method converts Frac to u32

--- projects/sin/src/main.rs
+++ projects/sin/src/main.rs
@@ -1,5 +1,5 @@
 use num::integer;
-use std::ops::{Add,Sub,Mul};
+use std::ops::{Add, Mul, Sub};
 
 fn main() {
     let sin1 = Frac::new(Interval::new(7761199951101802512, 7761199951101802513));
@@ -8,109 +8,98 @@ fn main() {
     let cos2 = cos1 * cos1 - sin1 * sin1;
     println!("{:?}", sin2);
     println!("{:?}", cos2);
-    println!("{:x}", integer::div_floor(sin1.num.min, 1<<31));
-    println!("{:x}", integer::div_floor(sin2.num.min, 1<<31));
-    let sin3 = sin1*cos2 + cos1*sin2;
-    let cos3 = cos1*cos2-sin1*sin2;
-    println!("{:x}", integer::div_floor(sin3.num.min, 1<<31));
-    let sin4 = sin2*cos2+cos2*sin2;
-    let cos4 = cos2*cos2-sin2*sin2;
-    println!("{:x}", integer::div_floor(-sin4.num.min, 1<<31));
+    let sin3 = sin1 * cos2 + cos1 * sin2;
+    let cos3 = cos1 * cos2 - sin1 * sin2;
+    let sin4 = sin2 * cos2 + cos2 * sin2;
+    let cos4 = cos2 * cos2 - sin2 * sin2;
+    println!("{:x}", sin1.to_u32());
+    println!("{:x}", sin2.to_u32());
+    println!("{:x}", sin3.to_u32());
+    println!("{:x}", sin4.to_u32());
 }
 
 #[derive(Copy, Clone, Debug)]
-struct Interval{
+struct Interval {
     min: i128,
     max: i128,
 }
 
-impl Interval{
-    fn new(min:i128, max:i128) -> Interval{
-        if min < 0 && 0 < max{
+impl Interval {
+    fn new(min: i128, max: i128) -> Interval {
+        if min < 0 && 0 < max {
             panic!("Cannot contain 0.");
         } else if min > max {
             panic!("min = {} < max = {} is not satisfied.", min, max);
         }
-        Interval{
-            min,
-            max
-        }
+        Interval { min, max }
     }
 
-    fn is_positive(&self) -> bool{
+    fn is_positive(&self) -> bool {
         self.min > 0
     }
 
-    fn sum(&self, other: &Interval) -> Interval{
-        Interval{
+    fn sum(&self, other: &Interval) -> Interval {
+        Interval {
             min: self.min + other.min,
             max: self.max + other.max,
         }
     }
-    fn sub(&self, other:&Interval) -> Interval{
-        Interval{
+    fn sub(&self, other: &Interval) -> Interval {
+        Interval {
             min: self.min - other.max,
             max: self.max - other.min,
         }
     }
-    fn prod(&self, other: &Interval) -> Interval{
+    fn prod(&self, other: &Interval) -> Interval {
         match (self.is_positive(), other.is_positive()) {
-            (true, true) => Interval::new(
-                self.min * other.min,
-                self.max * other.max,
-            ),
-            (true, false) => Interval::new(
-                self.max * other.min,
-                self.min * other.max,
-            ),
-            (false, true) => Interval::new(
-                self.min * other.max,
-                self.max * other.min,
-            ),
-            (false, false) => Interval::new(
-                self.max * other.max,
-                self.min * other.min
-            ),
+            (true, true) => Interval::new(self.min * other.min, self.max * other.max),
+            (true, false) => Interval::new(self.max * other.min, self.min * other.max),
+            (false, true) => Interval::new(self.min * other.max, self.max * other.min),
+            (false, false) => Interval::new(self.max * other.max, self.min * other.min),
         }
     }
 }
 
 #[derive(Debug, Copy, Clone)]
-struct Frac{
-    num: Interval
-    //denom: 2^63
+struct Frac {
+    num: Interval,//denom=2^63
 }
 
-const DENOM:i128 = 1<<63;
-
-impl Frac{
-    fn new(num: Interval) -> Frac{
-        Frac{
-            num
+impl Frac {
+    fn new(num: Interval) -> Frac {
+        Frac { num }
+    }
+    fn to_u32(&self) -> u32 {
+        let min = integer::div_floor(self.num.min.abs(), 1 << 31);
+        let max = integer::div_floor(self.num.max.abs(), 1 << 31);
+        if min == max {
+            min as u32
+        } else {
+            panic!("min={}, max={}", min, max);
         }
     }
 }
 
-impl Add for Frac{
+impl Add for Frac {
     type Output = Self;
-    fn add(self, other: Self) -> Frac{
+    fn add(self, other: Self) -> Frac {
         Frac::new(self.num.sum(&other.num))
     }
 }
 
-impl Sub for Frac{
+impl Sub for Frac {
     type Output = Self;
-    fn sub(self, other:Self) -> Frac{
+    fn sub(self, other: Self) -> Frac {
         Frac::new(self.num.sub(&other.num))
     }
 }
 
-impl Mul for Frac{
+impl Mul for Frac {
     type Output = Self;
-    fn mul(self, other:Self) -> Frac{
+    fn mul(self, other: Self) -> Frac {
         let num_prod = self.num.prod(&other.num);
-        let floor = integer::div_floor(num_prod.min, DENOM);
-        let ceil = integer::div_ceil(num_prod.max, DENOM);
+        let floor = integer::div_floor(num_prod.min, 1 << 63);
+        let ceil = integer::div_ceil(num_prod.max, 1 << 63);
         let interval = Interval::new(floor, ceil);
         Frac::new(interval)
     }

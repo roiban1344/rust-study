1942c61f32e1a19b263454ea93ec742d8fc4d303
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-10-22 22:45:18 +0900

    Try unit tests

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,25 +1,4 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
-}
+use rectangles::Rectangle;
 
 fn main() {
     let rect1 = Rectangle {

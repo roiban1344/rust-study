92de28fba3043052aa2806426a36df38dba567b3
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-10-23 17:32:50 +0900

    Split main function into parts

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,15 +1,44 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+        process::exit(1);
+    }
+}
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enouch arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
